605cb82b5def699e294e47b2b9b9c6706b809e17
John Pierce <pierce.johndaniel@gmail.com>
2021-06-09 11:46:43 -0500

    feat: added home controller for landing page

---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,10 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MathController {
+
+//    @RequestMapping("/add/{num}/and/{num}")
+}
