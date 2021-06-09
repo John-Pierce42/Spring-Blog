c901cb7c9c6333e6dba5835b0333e881aab21ea6
John Pierce <pierce.johndaniel@gmail.com>
2021-06-09 12:23:53 -0500

    feat: added and completed math controller exercise

--- src/main/java/com/codeup/springblog/MathController.java
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -1,10 +1,36 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
 
-//    @RequestMapping("/add/{num}/and/{num}")
+    @RequestMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public int add(@PathVariable int num1, @PathVariable int num2) {
+        return  num1 +  num2;
+    }
+
+    @RequestMapping("/subtract/{num1}/and/{num2}")
+    @ResponseBody
+    public int subtract(@PathVariable int num1, @PathVariable int num2) {
+        return num1 - num2;
+    }
+
+    @RequestMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public int multiply(@PathVariable int num1, @PathVariable int num2){
+        return num1 * num2;
+    }
+
+    @RequestMapping("/divide/{num1}/and/{num2}")
+    @ResponseBody
+    public int divide(@PathVariable int num1, @PathVariable int num2){
+        return num1 / num2;
+    }
+
+
 }
