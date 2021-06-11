41186e2dc7902c12dfd2d09d4a1d40f7642d3c27
John Pierce <pierce.johndaniel@gmail.com>
2021-06-11 10:28:13 -0500

    feat: added nav partial

--- src/main/java/com/codeup/springblog/DiceController.java
+++ src/main/java/com/codeup/springblog/DiceController.java
@@ -9,15 +9,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class DiceController {
 
     @GetMapping("/roll-dice")
-    Public String rollDive(){
+    public String rollDice(){
         return "dice/roll-dice";
     }
-}
-
-@GetMapping("/roll-dice/{guess}")
-public String diceGuess(@PathVariable int guess, Model model){
 
 
-    int random = Math.random() * 6;
+    @GetMapping("/roll-dice/{guess}")
+    public String diceGuess(@PathVariable int guess, Model model) {
+        String result;
 
+        int random = (int) Math.random() * 6 + 1;
+
+        if (guess == random) {
+            result = "your guess is correct!";
+        } else {
+            result = "Your guess is incorrect";
+        }
+
+        model.addAttribute("result", result);
+        model.addAttribute("guess", guess);
+        model.addAttribute("random", random);
+
+        return "dice/roll-dice-guess";
+    }
 }
