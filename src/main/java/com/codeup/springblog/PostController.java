99a5ea03578ceceb89cb8d391c22a240b289c33c
John Pierce <pierce.johndaniel@gmail.com>
2021-06-12 15:13:36 -0500

    feat: finished with controller exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,14 +1,47 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
 
-    @RequestMapping("/post")
+    @GetMapping("/posts")
     @ResponseBody
-    public String posts()
+    public String posts(){
+        return "posts/index.";
+    };
+
+    @GetMapping("/hello/{id}")
+    @ResponseBody
+    public String postId(@PathVariable long id){
+        return  "post/view";
+    };
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String getPostCreate(){
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String postCreate(){
+        return "posts/create";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
