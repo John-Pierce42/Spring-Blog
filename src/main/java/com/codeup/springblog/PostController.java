5d3f294e2290c1e5e71585d574d1d221be25535d
John Pierce <pierce.johndaniel@gmail.com>
2021-06-13 13:04:48 -0500

    feat:working through views exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,31 +1,34 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts(){
-        return "posts/index.";
+    public String posts(@PathVariable ArrayList<Model> model){
+
+//      model.add("post", post);
+
+        return "posts/index";
     };
 
     @GetMapping("/hello/{id}")
-    @ResponseBody
-    public String postId(@PathVariable long id){
-        return  "post/view";
+    public String postId(@PathVariable long id, Model model){
+        model.addAttribute("id", id);
+        return  "posts/show";
     };
 
     @GetMapping("/posts/create")
-    @ResponseBody
     public String getPostCreate(){
         return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    @ResponseBody
     public String postCreate(){
         return "posts/create";
     }
